a5a4cffe3e86e4b6392765d903361e88421bd1aa
Om Bhanushali <ombhanushali005@gmail.com>
2025-03-22 12:02:01 +0530

    feat: added login frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Login from "./pages/Login"
 
 const App = () => {
   return (
-    <div>App</div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        {/* Add other routes as needed */}
+        <Route path="/" element={<div>Home Page</div>} />
+        <Route path="/register" element={<div>Register Page</div>} />
+      </Routes>
+    </Router>
   )
 }
 
 export default App
+
