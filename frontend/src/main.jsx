07bd03a8d5bbc49f60a26dfaf8b3c3b0be61a38f
Vedant-0102 <vedantshinde164@gmail.com>
2025-03-22 18:34:57 +0530

    Upd: onboarding bento

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App2 from './App2'
 import './index.css'
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App2 />
+    <App />
   </React.StrictMode>
 )
