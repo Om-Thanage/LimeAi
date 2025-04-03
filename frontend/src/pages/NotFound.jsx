5fe7682513098f8f1d42ac88f96e0896f48cd8c5
Om Bhanushali <ombhanushali005@gmail.com>
2025-04-04 01:26:55 +0530

    chore: added assets to public

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Button } from '../components/Button'
 
 export default function NotFound() {
   return (
@@ -23,7 +22,7 @@ export default function NotFound() {
       </p>
       <Link 
         to="/"
-        className="mt-10 inline-flex items-center justify-center rounded-md bg-blue-600 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+        className="mt-10 inline-flex items-center justify-center rounded-md bg-blue-600 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-offset-2 focus-visible:outline-blue-600"
       >
         Go back home
       </Link>
